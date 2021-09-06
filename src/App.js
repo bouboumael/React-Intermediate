ad1ce06d68d92705d7211b512ae9b611ef07a903
bouboumael <bouboumael@orange.fr>
2021-09-06 11:56:15 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import DisplayEmployee from "./components/DisplayEmployee";
+import {sampleEmployee} from "./data/data";
+import {useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [employee, setEmployee] = useState(sampleEmployee)
+    const getEmployee = () => {
+        axios
+            .get('https://randomuser.me/api?nat=en')
+            // Extract the DATA from the received response
+            .then((response) => response.data)
+            // Use this data to update the state
+            .then((data) => {
+                setEmployee(data.results[0]);
+            });
+    }
+
+    return (
+        <div className={'app'}>
+            <DisplayEmployee employee={employee}/>
+            <button type={"button"} onClick={getEmployee}>Get employee</button>
+        </div>
+    );
 }
 
 export default App;
